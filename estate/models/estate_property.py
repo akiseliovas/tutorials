71d4f4599c3bf9fdfc1522c4606ce339021016a2
Sasa <aleksandraskiseliovas@gmail.com>
2025-05-18 11:48:05 +0300

    Add custom list view columns

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,15 +5,15 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Real estate property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string='Title')
     active = fields.Boolean(default=True)
     description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=date_utils.add(fields.Date.today(), months=3))
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(copy=False, default=date_utils.add(fields.Date.today(), months=3), string='Available From')
+    expected_price = fields.Float(required=True, string='Expected Price')
+    selling_price = fields.Float(readonly=True, copy=False, string='Selling Price')
+    bedrooms = fields.Integer(default=2, string='Bedrooms')
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
